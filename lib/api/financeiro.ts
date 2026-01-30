9f92336e965b2b93c1a8419cd531a6d68e3544b1
Samuel <fsdomingoss09@gmail.com>
2026-01-30 18:40:04 -0300

    refactor(api): replace http utility with fetch API and enhance data handling refactor(prisma): expand models and enums for better data structure delete: remove unused cookie and http utility files

--- lib/api/financeiro.ts
+++ lib/api/financeiro.ts
@@ -1,69 +1,66 @@
-import http from "@/utils/http";
-import { DadosResultado, DiscrepanciesData, OverviewData, PatientsListData, ProceduresData, ProfessionalData } from "./types/financial";
+import {
+  DadosResultado,
+  DiscrepanciesData,
+  OverviewData,
+  PatientsListData,
+  ProceduresData,
+  ProfessionalData,
+} from "./types/financial";
 
 export const postProcessar = async (
-  formData: FormData
+  formData: FormData,
 ): Promise<DadosResultado> => {
-  const result = await http.post<DadosResultado>("/api/process", formData);
-  return result;
+  const result = await fetch("/api/process", {
+    method: "POST",
+    body: formData,
+  });
+  return result.json();
 };
 
 export const getSummary = async (
   month: number,
-  year: number
+  year: number,
 ): Promise<DadosResultado> => {
-  const result = await http.get<DadosResultado>(
-    `/api/process/?month=${month}&year=${year}`
-  );
-  return result;
+  const result = await fetch(`/api/process/?month=${month}&year=${year}`);
+  return result.json();
 };
 
 export const getOverview = async (
   month: number,
-  year: number
+  year: number,
 ): Promise<OverviewData> => {
-  const result = await http.get<OverviewData>(
-    `/api/overview/?month=${month}&year=${year}`
-  );
-  return result;
+  const result = await fetch(`/api/overview/?month=${month}&year=${year}`);
+  return result.json();
 };
 
 export const getProfessional = async (
   month: number,
-  year: number
+  year: number,
 ): Promise<ProfessionalData> => {
-  const result = await http.get<ProfessionalData>(
-    `/api/professional/?month=${month}&year=${year}`
-  );
-  return result;
+  const result = await fetch(`/api/professional/?month=${month}&year=${year}`);
+  return result.json();
 };
 
 export const getPatients = async (
   month: number,
-  year: number
+  year: number,
 ): Promise<PatientsListData> => {
-  const result = await http.get<PatientsListData>(
-    `/api/patient/?month=${month}&year=${year}`
-  );
-  return result;
+  const result = await fetch(`/api/patient/?month=${month}&year=${year}`);
+  return result.json();
 };
 
 export const getDiscrepancies = async (
   month: number,
-  year: number
+  year: number,
 ): Promise<DiscrepanciesData> => {
-  const result = await http.get<DiscrepanciesData>(
-    `/api/discrepancies/?month=${month}&year=${year}`
-  );
-  return result;
+  const result = await fetch(`/api/discrepancies/?month=${month}&year=${year}`);
+  return result.json();
 };
 
 export const getProcedures = async (
   month: number,
-  year: number
+  year: number,
 ): Promise<ProceduresData> => {
-  const result = await http.get<ProceduresData>(
-    `/api/procedures/?month=${month}&year=${year}`
-  );
-  return result;
+  const result = await fetch(`/api/procedures/?month=${month}&year=${year}`);
+  return result.json();
 };
